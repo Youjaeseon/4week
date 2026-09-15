60f857cf9f03dfab8f32767158943eda00db2a07
Youjaeseon <wotjd0701@naver.com>
2026-09-15 12:50:23 +0000

    Q4 solve

--- data_structures_docker/Data-Structures/Binary_Tree/Q4_E_BT.c
+++ data_structures_docker/Data-Structures/Binary_Tree/Q4_E_BT.c
@@ -103,7 +103,16 @@ int main()
 int sumOfOddNodes(BTNode *node)
 
 {
-    /* add your code here */
+    if(node == NULL)
+        return 0;
+    
+    int odd = 0;
+
+    if(node->item%2 != 0)
+        odd += node->item;
+
+    return sumOfOddNodes(node->left)+sumOfOddNodes(node->right)+odd;
+
 }
 
 //////////////////////////////////////////////////////////////////////////////////
