5bfcf74063fb901e31d5d19d45471c3ce476074b
Youjaeseon <wotjd0701@naver.com>
2026-09-15 11:33:57 +0000

    Q3 solve

--- data_structures_docker/Data-Structures/Binary_Tree/Q3_E_BT.c
+++ data_structures_docker/Data-Structures/Binary_Tree/Q3_E_BT.c
@@ -101,7 +101,17 @@ int main()
 int countOneChildNodes(BTNode *node)
 
 {
-    /* add your code here */
+    if(node == NULL)
+        return 0;
+    
+    int left = countOneChildNodes(node->left);
+    int right = countOneChildNodes(node->right);
+
+    if((node->left != NULL && node->right == NULL)||(node->left == NULL && node->right != NULL)){
+        return left + right + 1;
+    }
+    return left + right;
+
 }
 
 ///////////////////////////////////////////////////////////////////////////////////
